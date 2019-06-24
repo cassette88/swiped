caa27df1ae956fa1381412a1d7ad07325a242bf9
Dave <chopin@iJones.local>
2019-06-24 11:15:33 -0400

    init commit

---
+++ src/Swipe.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Swipe extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Swipe to the right</h3>
+            </div>
+        )
+    }
+}
