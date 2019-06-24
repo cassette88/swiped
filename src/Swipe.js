866721ea82a47ee9494672d27712c0272ebb96b7
Dave <chopin@iJones.local>
2019-06-24 14:51:09 -0400

    added swipeable events

--- src/Swipe.js
+++ src/Swipe.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
+import SwipeableViews from 'react-swipeable-views';
+import Fade from 'react-reveal/Fade';
+import './Swipe.css'
+
 
 export default class Swipe extends Component {
     render() {
         return (
-            <div>
-                <h3>Swipe to the right</h3>
-            </div>
+            
+            <SwipeableViews enableMouseEvents resistance>
+               <div> <h3 className="fade-in">Slide #1</h3></div>
+               <div><h3> Slide #2</h3></div>
+               <div><h3 className="fade-in">Slide #3 </h3></div>
+            </SwipeableViews>
+           
         )
     }
 }
